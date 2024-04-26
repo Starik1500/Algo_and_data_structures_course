03823d34c378d1171c7b9f936d607034b9eaf48d
Vitaliy <69012012+Starik1500@users.noreply.github.com>
2024-04-26 12:33:48 +0300

    Оновив тести

--- test/Test_matrix_bfs.py
+++ test/Test_matrix_bfs.py
@@ -1,5 +1,11 @@
 import unittest
-from matrix_bfs import bfs, main, is_valid, create_coordinate, create_queue_node
+import os
+import sys
+test_file_path = os.path.abspath(__file__)
+common_parent_path = os.path.abspath(os.path.join(os.path.dirname(test_file_path), os.pardir))
+src_path = os.path.join(common_parent_path, 'src')
+sys.path.append(src_path)
+from matrix_bfs import bfs, main, is_valid
 
 
 class TestShortestPath(unittest.TestCase):
