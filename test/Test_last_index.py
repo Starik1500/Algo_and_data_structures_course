222c20619e3dae403ff8d785d42fa3814f3fb766
Vitaliy <69012012+Starik1500@users.noreply.github.com>
2024-05-17 11:16:22 +0300

    Update Test_last_index.py

--- test/Test_last_index.py
+++ test/Test_last_index.py
@@ -5,7 +5,7 @@ test_file_path = os.path.abspath(__file__)
 common_parent_path = os.path.abspath(os.path.join(os.path.dirname(test_file_path), os.pardir))
 src_path = os.path.join(common_parent_path, 'src')
 sys.path.append(src_path)
-from The_end_index_with_native_method import pattern_search
+from last_index import pattern_search
 
 class TestPatternSearch(unittest.TestCase):
     def test_pattern_search(self):
